127effbecdcec6968d7406a9aafda1a145d798bd
Yaroslav Zaprudskii <slavoyarmc@gmail.com>
2021-05-27 22:37:22 +0300

    добавил комментарии у js скрипта

--- script.js
+++ script.js
@@ -1,38 +1,52 @@
 
-
+//включение\выключение таймера взависимости от текущего состояния
+//таймер и соответствующая кнопка передавются в фукцию
 function change_enable(btn, timer_id){
+	//получение элемента таймера по id
 	const timer = document.getElementById(timer_id);
+
+	//получение текущего статуса таймера
 	const enabled = (timer.getAttribute("enabled")=="true");
 
+	//получение текущего времени и приведение его к формату подобному в полях timeSensor
 	const now = Date.now()/1000;
+	//получение значений атрибутов time и startTime у таймера
 	const time = timer.getAttribute("time");
 	const start_time = timer.getAttribute("startTime");
+
 	const change = time-start_time;
 
+	//переключение статуса работы таймера взависимости от текущего
 	if (!enabled){
+		//назначение времени для избежания рывка анимации
 		timer.setAttribute("startTime", (now-change).toString());
 		timer.setAttribute("enabled", "true");
 		btn.innerHTML = "Стоп";
 	}
 	else{
-
-		timer.setAttribute("stopped", time);
 		timer.setAttribute("enabled", "false");
 		btn.innerHTML = "Старт";
 	}	
 }
 
+//получение случайного числа в пределах min max
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max-min)+min);
 }
 
+//функция генерации подушки
 function pillowGenerator(){
+	//количество узлов из которых состоит подушка
 	const DIM = 25;
+	//количество узлов поднятия подушки с 0 до height
 	const ELEV = Math.floor(DIM/5);
+	//условная высота подушки
 	const HEIGHT = 4;
 
+	//массив высот для elevationGrid
 	let arr = [];
 
+	//заполнение массива 0
 	for (let i=0;i<DIM;i++){
 		let sub_arr = [];
 		for(let j=0;j<DIM;j++){
@@ -41,10 +55,12 @@ function pillowGenerator(){
 		arr.push(sub_arr);
 	}
 
+	//переменные для шага инкрементации высоты, начальное значение и мулитипликатор (для контроля высоты) 
 	let inc = HEIGHT/ELEV;
 	let val = 0;
 	let mult = 0.25;
 
+	//вычисление высот для сторон подушки
 	for (let i=0;i<ELEV;i++){
 		for (let j = ELEV;j < DIM-ELEV;j++){
 			if (i){
@@ -61,8 +77,11 @@ function pillowGenerator(){
 		val += inc;
 	}
 
+	//середина массива
 	c = DIM/2;
 
+	//создание углубления для короны
+	//уменьшение где координаты соответствуют формуле 25<(x^2+y^2)<36
 	for(let i=ELEV; i<DIM-ELEV; i++){
 		for(let j = ELEV; j<DIM-ELEV; j++){
 			arr[i][j] = val;
@@ -73,6 +92,7 @@ function pillowGenerator(){
 		}
 	}
 
+	//вычисление высот углов подушки для elevationGrid
 	val = 0;
 	for (let i = 0; i < ELEV; i++){
 		val = 0;
@@ -87,6 +107,7 @@ function pillowGenerator(){
 		}
 	}
 
+	//перевод массива в string
 	let str = '';
 	for (let i = 0; i < DIM; i++){
 		for (let j = 0; j < DIM; j++){
@@ -94,9 +115,11 @@ function pillowGenerator(){
 		}
 	}
 
+	//перевод из условных едениц в размеры x3dom
 	const size = 40;
 	let spacing = size/DIM;
 
+	//получение элемента Pillow и установка соответствующих полей
 	const pillow = document.getElementById("Pillow");
 	pillow.setAttribute("height", str);
 	pillow.setAttribute("xDimension", DIM.toString());
